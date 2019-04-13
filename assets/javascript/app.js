13baa546ea2a7eb76171fc80a6abb61b6baf7043
Patrick Owens <manbearpat@gmail.com>
2019-04-13 10:22:05 -0700

    added images

---
+++ assets/javascript/app.js
@@ -0,0 +1,2 @@
+//on the clock gif
+// https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjwtDKx83hAhVJgK0KHT8XAAIQjRx6BAgBEAU&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fclock-gifs&psig=AOvVaw30P9GAAI2_PYdieG5tNSuP&ust=1555261721282050
